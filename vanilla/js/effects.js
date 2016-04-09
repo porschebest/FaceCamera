3d8e9bc75f754fd554983dfe7bd2a88803a0c62a
porschebest <porschebestcar@gmail.com>
2016-04-09 20:38:07 +0800

    add new modules

---
+++ vanilla/js/effects.js
@@ -0,0 +1,108 @@
+/*
+ *  Predefined filter effects with names for PhotoFilter.js
+ *
+ *  Last modified: March 1, 2014
+ */
+
+// Prepare image-layer effects
+
+var rockstarLayer = new Image();
+rockstarLayer.src = 'images/effects/KF_V.png';
+
+var rockstarLayer2 = new Image();
+rockstarLayer2.src = 'images/effects/KF_V.png';
+
+
+
+// Define named effects
+
+var ApplyEffects = {
+	reset: function(img, format) {
+		var f = new PhotoFilter(img, format);
+		f.reset();
+	},
+	fluorescent: function(img, format) {
+		// Warm, saturated tones with an emphasis on yellow
+		var f = new PhotoFilter(img, format);
+		f.filterImage('rgbAdjust', [1, 1.2, 1.4]);
+		f.filterImage('brightness', 10);
+		f.render();
+	},
+	nostalgia: function(img, format) {
+		// Slightly blurred, with sepia tone
+		var f = new PhotoFilter(img, format);
+		f.filterImage('rgbAdjust', [1.4, 1.2, 1]);
+		f.filterImage('convolute',
+			[ 0.1, 0.1, 0.1,
+			  0.1, 0.1, 0.1,
+			  0.1, 0.1, 0.1 ]
+	    );
+	    f.render();
+	},
+	phykos: function(img, format) {
+		// Slightly blurred, with yellow and green saturated
+		var f = new PhotoFilter(img, format);
+		f.filterImage('rgbAdjust', [1, 1.1, 1.1]);
+		f.filterImage('convolute',
+			[ 0.1, 0.1, 0.1,
+			  0.1, 0.1, 0.1,
+			  0.1, 0.1, 0.1 ]
+	    );
+	    f.render();
+	},
+	lotus: function(img, format) {
+		// Sepia-like, with an emphasis on purples and browns
+		var f = new PhotoFilter(img, format);
+		f.filterImage('rgbAdjust', [1.4, 1.15, 1.1]);
+		f.render();
+	},
+	memphis: function(img, format) {
+		// Sharp images with a magenta-meets-purple tint
+		var f = new PhotoFilter(img, format);
+		f.filterImage('rgbAdjust', [1.2, 1, 1.1]);
+		f.filterImage('convolute',
+			[ 0, -1,  0,
+	    	 -1,  5, -1,
+	    	  0, -1,  0 ]
+	    );
+	    f.render();
+	},
+	deutlich: function(img, format) {
+		// High exposure
+		var f = new PhotoFilter(img, format);
+		f.filterImage('convolute',
+			[ 0, 0, 0,
+	    	  0, 1.3, 0,
+	    	  0, 0, 0 ]
+	    );
+	    f.render();
+	},
+	sumie: function(img, format) {
+		var f = new PhotoFilter(img, format);
+		f.filterImage('grayscale');
+		f.render();
+	},
+	rockstar: function(img, format) {
+		// applying a starry layer
+		var f = new PhotoFilter(img, format);
+		f.applyLayer(rockstarLayer, rockstarLayer2);
+		f.render();
+
+	}
+
+
+
+//    var ctx=new Img();
+//	ctx.src='images/effects/KF_V.png';
+//	ctx.fillRect(50,50,50,10);
+//	ctx.globalCompositeOperation=gco[n];
+//	ctx.beginPath();
+//	ctx.fillStyle="red";
+//	ctx.arc(50,50,30,0,2*Math.PI);
+//	ctx.fill();
+//  var c=document.getElementById("myCanvas");
+//    f.applyLayer(ctx);
+//    f.render();
+//  }
+
+};
